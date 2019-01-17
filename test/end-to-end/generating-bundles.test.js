c70ae4efae701a276025514edcc5ed73eb03950f
Jake Champion <jakechampion.jake2@googlemail.com>
2019-01-17 10:21:39 +0000

    Add end to end test for generating polyfill bundles in the same order

---
+++ test/end-to-end/generating-bundles.test.js
@@ -0,0 +1,30 @@
+/* eslint-env mocha */
+'use strict';
+
+const assert = require('proclaim');
+const PolyfillLibrary = require('../../');
+const polyfillLibrary = new PolyfillLibrary;
+
+describe("polyfill-library", function () {
+    this.timeout(30000);
+
+    it('should produce same output for same bundle', async () => {
+        const bundle1 = await polyfillLibrary.getPolyfillString({
+            features: {
+                all: {}
+            },
+            uaString: 'other/0.0.0',
+            unknown: 'polyfill'
+        });
+
+        const bundle2 = await polyfillLibrary.getPolyfillString({
+            features: {
+                all: {}
+            },
+            uaString: 'other/0.0.0',
+            unknown: 'polyfill'
+        });
+
+        assert.deepStrictEqual(bundle1, bundle2);
+    });
+});
