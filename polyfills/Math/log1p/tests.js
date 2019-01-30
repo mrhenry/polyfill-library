754d774e3913a5baab905ee7c5ce7b7a38ffd4d0
Jake Champion <me@jakechampion.name>
2019-01-30 17:13:38 +0000

    Update polyfills/Math/log1p/tests.js

--- polyfills/Math/log1p/tests.js
+++ polyfills/Math/log1p/tests.js
@@ -14,7 +14,7 @@ it('has correct name', function () {
 });
 
 it('is not enumerable', function () {
-	proclaim.nonEnumerable(Math, 'log1p');
+	proclaim.isNotEnumerable(Math, 'log1p');
 });
 
 it('works as expected when called with no arguments', function () {
