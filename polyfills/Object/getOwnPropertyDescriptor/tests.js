bea1701b6407c397c915d8381a0e0597d9017dc0
Jake Champion <jakechampion.jake2@googlemail.com>
2019-01-31 10:54:41 +0000

    Use correct method name

--- polyfills/Object/getOwnPropertyDescriptor/tests.js
+++ polyfills/Object/getOwnPropertyDescriptor/tests.js
@@ -14,7 +14,7 @@ it('has correct name', function () {
 });
 
 it('is not enumerable', function () {
-	proclaim.nonEnumerable(Object, 'getOwnPropertyDescriptor');
+	proclaim.isNotEnumerable(Object, 'getOwnPropertyDescriptor');
 });
 
 var propertyDescriptorsSupported = (function () {
