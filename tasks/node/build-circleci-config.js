0c1daed2c9c5b039752fc0e919cd5e40f6dc710d
Jake Champion <jakechampion.jake2@googlemail.com>
2019-02-01 16:30:19 +0000

    Add command timeout of an hour for testing on browsers

--- tasks/node/build-circleci-config.js
+++ tasks/node/build-circleci-config.js
@@ -14,19 +14,26 @@ const polyfillsWhichHaveTests = globby.sync(['polyfills/**/tests.js', '!polyfill
 
 _.chunk(polyfillsWhichHaveTests, 10).map(polyfillsWhichHaveTests => {
     const testCommands = polyfillsWhichHaveTests.map(feature => {
-        return {run:`node ./test/polyfills/test-individual-feature.js ${feature}`};
+        return {
+            run: {
+                name: `Testing ${feature}`,
+                no_output_timeout: "60m",
+                command: `node ./test/polyfills/test-individual-feature.js ${feature}`
+            }
+        };
     });
     return {
         docker: [{
             image: 'circleci/node:10'
         }],
         steps: [
-        'checkout',
-        {
-            run: 'npm ci'
-        },
-        ...testCommands
-    ]};
+            'checkout',
+            {
+                run: 'npm ci'
+            },
+            ...testCommands
+        ]
+    };
 }).forEach((job, index) => {
     const jobName = `test_${index}`;
     circleConfig.jobs[jobName] = job;
@@ -45,7 +52,7 @@ _.chunk(polyfillsWhichHaveTests, 10).map(polyfillsWhichHaveTests => {
     });
 });
 fs.writeFileSync(
-    path.join(__dirname, '../../.circleci/config.yml'), 
+    path.join(__dirname, '../../.circleci/config.yml'),
     `
 # Do not modify this file directly, it is built by ./tasks/node/build-circleci-config.js.
 ${yaml.stringify(circleConfig)}
