f732541af544b5d3856c29ec74c7372aae14c2a6
Jake Champion <jakechampion.jake2@googlemail.com>
2019-02-21 16:41:41 +0000

    Run more tests in each job to avoid hitting the abuse detection in circleci

--- tasks/node/build-circleci-config.js
+++ tasks/node/build-circleci-config.js
@@ -12,7 +12,7 @@ const polyfillsWhichHaveTests = globby.sync(['polyfills/**/tests.js', '!polyfill
     transform: (entry) => entry.replace('polyfills/', '').replace('/tests.js', '').replace(/\//g, '.')
 });
 
-_.chunk(polyfillsWhichHaveTests, 10).map(polyfillsWhichHaveTests => {
+_.chunk(polyfillsWhichHaveTests, 14).map(polyfillsWhichHaveTests => {
     const testCommands = polyfillsWhichHaveTests.map(feature => {
         return {
             run: {
