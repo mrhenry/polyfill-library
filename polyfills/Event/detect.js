987630a085e29226da16b5dc542042c687560191
Jake Champion <jakechampion.jake2@googlemail.com>
2019-04-03 12:07:11 +0100

    Removing the typeof check with early return because it is handled via the `new Event` check, which is more thorough and will correctly detect bad Event constructors in Android 4.x
    Fixes https://github.com/Financial-Times/polyfill-library/issues/19

--- polyfills/Event/detect.js
+++ polyfills/Event/detect.js
@@ -1,11 +1,10 @@
 (function(global) {
 
 	if (!('Event' in global)) return false;
-	if (typeof global.Event === 'function') return true;
 
 	try {
 
-		// In IE 9-11, the Event object exists but cannot be instantiated
+		// In IE 9-11 and Android 4.x, the Event object exists but cannot be instantiated
 		new Event('click');
 		return true;
 	} catch(e) {
