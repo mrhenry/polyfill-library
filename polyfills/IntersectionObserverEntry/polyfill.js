d211581eb4c78b4e6e2aa5a07eb2bea1ea89cd41
Jake Champion <me@jakechampion.name>
2019-07-12 10:24:00 +0100

    Update polyfill.js

--- polyfills/IntersectionObserverEntry/polyfill.js
+++ polyfills/IntersectionObserverEntry/polyfill.js
@@ -1,8 +1,9 @@
 // Minimal polyfill for Edge 15's lack of `isIntersecting`
 // See: https://github.com/w3c/IntersectionObserver/issues/211
-Object.defineProperty(window.IntersectionObserverEntry.prototype,
+Object.defineProperty(IntersectionObserverEntry.prototype,
 	'isIntersecting', {
 		get: function () {
 			return this.intersectionRatio > 0;
 		}
-});
+	}
+);
