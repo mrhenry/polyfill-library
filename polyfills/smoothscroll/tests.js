a0285ff44bd0ba50e6adb0f05b5bd9d09d728003
Nick Randall <nicksrandall@gmail.com>
2019-10-30 11:25:53 -0400

    [NR] fixing tests

--- polyfills/smoothscroll/tests.js
+++ polyfills/smoothscroll/tests.js
@@ -7,6 +7,8 @@ describe('scroll', function () {
     });
 
     it('is defined as a function on Element.prototype', function () {
+        // The polyfill only patches HTMLElement if available.
+        var Element = window.HTMLElement || window.Element;
         proclaim.isTypeOf(Element.prototype.scroll, 'function');
     });
 });
@@ -17,12 +19,14 @@ describe('scrollBy', function () {
     });
 
     it('is defined as a function on Element.prototype', function () {
+        var Element = window.HTMLElement || window.Element;
         proclaim.isTypeOf(Element.prototype.scrollBy, 'function');
     });
 });
 
 describe('scrollIntoView', function () {
     it('is defined as a function on Element.prototype', function () {
+        var Element = window.HTMLElement || window.Element;
         proclaim.isTypeOf(Element.prototype.scrollIntoView, 'function');
     });
 });
