31cd6634e0846355dbf75dc9dee1f56c6b414ce0
Jake Champion <me@jakechampion.name>
2020-01-23 01:03:04 +0000

    simplify circle config
    remove requires which are redundant since the test_0 already has them and all the other test tasks require test_0

--- tasks/node/build-circleci-config.js
+++ tasks/node/build-circleci-config.js
@@ -48,7 +48,7 @@ _.chunk(polyfillsWhichHaveTests, 14).map(polyfillsWhichHaveTests => {
                     ignore: "master"
                 }
             },
-            requires: ["check_circle_config", "lint_js", "unit_tests"].concat(index > 0 ? previousJob:[])
+            requires: index === 0 ? ["check_circle_config", "lint_js", "unit_tests"] : [previousJob]
         }
     });
 });
