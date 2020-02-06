415c1629a683ec2a2a6a04448696f60a22b9ed2a
Jake Champion <me@jakechampion.name>
2020-02-06 10:26:51 +0000

    Add a better focusin detect

--- polyfills/Event/focusin/detect.js
+++ polyfills/Event/focusin/detect.js
@@ -1,1 +1,18 @@
-"onfocusin" in window
+(function() {
+	var support = false;
+	var parent = document.lastChild;
+    var a = document.createElement('a');
+	var addSupport = function () {
+		support = true;
+	};
+
+	a.href = '#';
+
+	a.addEventListener ? a.addEventListener('focusin', addSupport) : a.onfocusin = addSupport;
+
+	parent.appendChild(a).focus();
+
+	parent.removeChild(a);
+
+	return support;
+})()
