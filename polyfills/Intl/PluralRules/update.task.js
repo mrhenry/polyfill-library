9745fe109e672addfe038c4a7b2ab1de28739502
Romain Menke <11521496+romainmenke@users.noreply.github.com>
2020-02-29 13:46:52 +0100

    intl update script : fix detect (#519)

--- polyfills/Intl/PluralRules/update.task.js
+++ polyfills/Intl/PluralRules/update.task.js
@@ -61,7 +61,7 @@ function intlLocaleDetectFor(locale) {
 	return "'Intl' in this && " +
 			"Intl.PluralRules && " +
 			"Intl.PluralRules.supportedLocalesOf && " +
-			'(function() { try { return Intl.PluralRules.supportedLocalesOf("' + locale + '").length === 1; } catch (e) { return false; } })';
+			'(function() { try { return Intl.PluralRules.supportedLocalesOf("' + locale + '").length === 1; } catch (e) { return false; } }())';
 }
 
 console.log('Importing Intl.PluralRules~locale.* polyfill from ' + LocalesPath);
