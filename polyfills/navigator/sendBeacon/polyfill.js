15f9a44928b8b1f4627ae267a245581245f57e88
Jake Champion <jakechampion.jake2@googlemail.com>
2020-03-04 11:08:07 +0000

    If navigator.sendBeacon can not queue the work for transferring, return false as per the specification. (#524)
    Fixes https://github.com/Financial-Times/polyfill-service/issues/2160

--- polyfills/navigator/sendBeacon/polyfill.js
+++ polyfills/navigator/sendBeacon/polyfill.js
@@ -1,16 +1,20 @@
-/* global ActiveXObject */
 if (!('navigator' in self)) self.navigator = {};
 self.navigator.sendBeacon = function sendBeacon(url, data) {
-	var xhr = ('XMLHttpRequest' in window) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-	xhr.open('POST', url, false);
-	xhr.setRequestHeader('Accept', '*/*');
-	if (typeof data === 'string') {
-		xhr.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
-	} else if (Object.prototype.toString.call(data) === '[object Blob]') {
-		if (data.type) {
-			xhr.setRequestHeader('Content-Type', data.type);
+	try {
+		var xhr = new XMLHttpRequest();
+		xhr.open('POST', url, false);
+		xhr.onerror = function() {};
+		xhr.setRequestHeader('Accept', '*/*');
+		if (typeof data === 'string') {
+			xhr.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
+		} else if (Object.prototype.toString.call(data) === '[object Blob]') {
+			if (data.type) {
+				xhr.setRequestHeader('Content-Type', data.type);
+			}
 		}
+		xhr.send(data);
+		return true;
+	} catch (error) {
+		return false;
 	}
-	xhr.send(data);
-	return true;
 };
