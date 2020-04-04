50dc25566fcaeba48d6787b342e7c8d9ac827e1b
Jake Champion <me@jakechampion.name>
2020-04-05 00:01:42 +0100

    move tests which don't require Symbol outside of the symbol if block

--- polyfills/Object/fromEntries/tests.js
+++ polyfills/Object/fromEntries/tests.js
@@ -39,32 +39,50 @@ it('throws when an entry object is absent', function () {
     }, TypeError);
 });
 
-if('Symbol' in self && 'iterator' in self.Symbol && !!Array.prototype[Symbol.iterator]) {
-    it('returns empty object if given an empty array', function () {
-        proclaim.deepStrictEqual(Object.fromEntries([]), {});
-    });
-    
-    if ('getOwnPropertyDescriptor' in Object) {
-        it('Creates data properties which are enumerable, writable, and configurable', function () {
-            var result = Object.fromEntries([
-                ['key', 'value']
-            ]);
-            proclaim.deepStrictEqual(Object.getOwnPropertyDescriptor(result, "key"), {
-                value: 'value',
-                enumerable: true,
-                writable: true,
-                configurable: true
-            });
+it('returns empty object if given an empty array', function () {
+    proclaim.deepStrictEqual(Object.fromEntries([]), {});
+});
+
+if ('getOwnPropertyDescriptor' in Object) {
+    it('Creates data properties which are enumerable, writable, and configurable', function () {
+        var result = Object.fromEntries([
+            ['key', 'value']
+        ]);
+        proclaim.deepStrictEqual(Object.getOwnPropertyDescriptor(result, "key"), {
+            value: 'value',
+            enumerable: true,
+            writable: true,
+            configurable: true
         });
-    }
-    
-    it('succeeds when an entry object is a boxed Object', function () {
-        proclaim.deepStrictEqual(Object.fromEntries([Object('ab')]).a, 'b');
     });
-    
-    it('succeeds when an entry object is a boxed String', function () {
-        proclaim.deepStrictEqual(Object.fromEntries([new String('ab')]).a, 'b');
+}
+
+it('succeeds when an entry object is a boxed Object', function () {
+    proclaim.deepStrictEqual(Object.fromEntries([Object('ab')]).a, 'b');
+});
+
+it('succeeds when an entry object is a boxed String', function () {
+    proclaim.deepStrictEqual(Object.fromEntries([new String('ab')]).a, 'b');
+});
+
+it('works with expected input', function () {
+    var a = {};
+    var b = {};
+    var c = {};
+    var entries = [
+        ['a', a],
+        ['b', b],
+        ['c', c]
+    ];
+
+    proclaim.deepStrictEqual(Object.fromEntries(entries), {
+        a: a,
+        b: b,
+        c: c
     });
+});
+
+if('Symbol' in self && 'iterator' in self.Symbol && !!Array.prototype[Symbol.iterator]) {
     it('works with Symbols', function () {
         var key = Symbol();
         var result = Object.fromEntries([
@@ -72,21 +90,5 @@ if('Symbol' in self && 'iterator' in self.Symbol && !!Array.prototype[Symbol.ite
         ]);
         proclaim.deepStrictEqual(result[key], 'value');
     });
-    
-    it('works with expected input', function () {
-        var a = {};
-        var b = {};
-        var c = {};
-        var entries = [
-            ['a', a],
-            ['b', b],
-            ['c', c]
-        ];
-        
-        proclaim.deepStrictEqual(Object.fromEntries(entries), {
-            a: a,
-            b: b,
-            c: c
-        });
-    });
+
 }
