c104006fd88e7d778ce2b0c74f8600983fcb779b
Long Ho <longlho@users.noreply.github.com>
2020-06-08 14:15:15 -0400

    fix tests

--- polyfills/Intl/getCanonicalLocales/tests.js
+++ polyfills/Intl/getCanonicalLocales/tests.js
@@ -3,6 +3,6 @@
 
 describe('Intl.getCanonicalLocales', function () {
   it('should be able to resolve locale list', function () {
-    proclaim.deepEqual(new Intl.getCanonicalLocales('en'), ['en']);
+    proclaim.deepEqual(Intl.getCanonicalLocales('en'), ['en']);
   });
 });
