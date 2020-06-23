3327ff93b3e7521e3fd93e18ac21ef3c48b71f3d
Marshall Cheng <marshallycheng@gmail.com>
2020-06-23 16:37:35 -0700

    Remove encoding test as library doesnt include

--- polyfills/TextEncoder/tests.js
+++ polyfills/TextEncoder/tests.js
@@ -5,14 +5,6 @@ describe("TextEncoder", function() {
   it("has a valid constructor", function() {
     proclaim.isInstanceOf(new TextEncoder(), TextEncoder);
   });
-
-  describe("#encoding", function() {
-    it("returns 'utf-8' as the encoding", function() {
-      var encoder = new TextEncoder();
-
-      proclaim.equal(encoder.encoding, "utf-8");
-    });
-  });
 });
 
 describe("TextDecoder", function() {
