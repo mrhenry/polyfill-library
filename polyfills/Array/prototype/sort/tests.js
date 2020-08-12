a836b04599ead9e6a3a51afe8dcb67489b7f10b9
Jake Champion <me@jakechampion.name>
2020-08-12 17:03:04 +0100

    Add a stable Array.prototype.sort polyfill

---
+++ polyfills/Array/prototype/sort/tests.js
@@ -0,0 +1,32 @@
+/* globals proclaim */
+
+it("is a function", function () {
+	proclaim.isFunction(Array.prototype.sort);
+});
+
+it("has correct arity", function () {
+	proclaim.arity(Array.prototype.sort, 1);
+});
+
+it("has correct name", function () {
+	proclaim.hasName(Array.prototype.sort, "sort");
+});
+
+it("is not enumerable", function () {
+	proclaim.isNotEnumerable(Array.prototype, "sort");
+});
+
+it("has a stable sort", function () {
+	var obj = {
+		length: 3,
+		0: 2,
+		1: 1,
+		2: 3
+	};
+	proclaim.deepStrictEqual(
+		Array.prototype.sort.call(obj, function (a, b) {
+			return a - b;
+		}),
+		obj
+	);
+});
