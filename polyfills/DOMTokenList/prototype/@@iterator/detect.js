399840aa63178e124c88a0822c9e561ae929bf1e
romainmenke <romainmenke@gmail.com>
2020-10-16 11:34:13 +0200

    fix : polyfills/DOMTokenList/prototype/@@iterator/detect.js

--- polyfills/DOMTokenList/prototype/@@iterator/detect.js
+++ polyfills/DOMTokenList/prototype/@@iterator/detect.js
@@ -1,7 +1,7 @@
 'Symbol' in self && 'iterator' in self.Symbol && (function(){
     try {
         var div = document.createElement('div');
-        return div.classList && div.classList[self.Symbol.iterator];
+        return !!(div.classList && div.classList[self.Symbol.iterator]);
     } catch (err) {
         return false;
     }
