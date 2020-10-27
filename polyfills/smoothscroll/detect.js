26145208da5ed1701288478be22b69f7196d5ba9
kholiavko-roman <roman.kholiavko@moovweb.com>
2020-10-27 15:55:06 +0200

    FIX: smoothscroll detect.

--- polyfills/smoothscroll/detect.js
+++ polyfills/smoothscroll/detect.js
@@ -1,1 +1,32 @@
-'document' in self && 'documentElement' in self.document && 'style' in self.document.documentElement && 'scrollBehavior' in document.documentElement.style;
+('document' in self && 'documentElement' in self.document && 'style' in self.document.documentElement && 'scrollBehavior' in document.documentElement.style) || (function () {
+  try {
+    var supportsSmoothScroll = false;
+
+    var scrollOptions = {
+      top: 1,
+      left: 0
+    };
+
+    Object.defineProperty(scrollOptions, 'behavior', {
+      get: function () {
+        supportsSmoothScroll = true;
+        return 'smooth';
+      },
+
+      // Ensure this property lasts through cloning / destructuring:
+      enumerable: true
+    });
+
+    var a = document.createElement('DIV');
+    var b = document.createElement('DIV');
+    a.setAttribute('style', 'height: 1px; overflow: scroll;');
+    b.setAttribute('style', 'height: 2px; overflow: scroll;');
+
+    a.appendChild(b);
+    a.scrollTo(scrollOptions);
+
+    return supportsSmoothScroll;
+  } catch (e) {
+    return false;
+  }
+})();
