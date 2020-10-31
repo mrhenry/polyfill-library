113a0a6175d809d743667195ae0e3148629fb792
romainmenke <romainmenke@gmail.com>
2020-10-31 16:13:20 +0100

    cleanup

--- polyfills/AbortController/tests.js
+++ polyfills/AbortController/tests.js
@@ -1,12 +1,5 @@
 /* globals proclaim */
 
-beforeEach(function (done) {
-    // 
-    setTimeout(function () {
-        done();
-    }, 50);
-})
-
 describe('AbortSignal', function () {
     // Because these tests make use of the network, they can be unreliable and fail for reasons outside of the polyfills concern such as a Network Timeout.
     // We increase mocha timeout from 2 seconds to 10 seconds to cater for slow responses.
