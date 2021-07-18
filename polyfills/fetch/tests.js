df1aa52b101250d7fd992acfc3b42d6180b9ac27
romainmenke <romainmenke@gmail.com>
2021-07-18 09:54:58 +0200

    fix URL / fetch interop

--- polyfills/fetch/tests.js
+++ polyfills/fetch/tests.js
@@ -6,3 +6,29 @@
 it('exists', function () {
 	proclaim.ok('fetch' in self);
 });
+
+if ('URLSearchParams' in self) {
+	it('works with URLSearchParams as Request body', function (done) {
+		// https://github.com/Financial-Times/polyfill-library/issues/870
+		var request = new Request('#', { body: new URLSearchParams({ foo: 'baz' }), method: 'POST' });
+		if (!('text' in request)) {
+			proclaim.fail('expected "text" function in Request');
+			return;
+		}
+
+		var text = request.text();
+		if (!('then' in text)) {
+			proclaim.fail('expected "then" function in text body');
+			return;
+		}
+
+		text.then(function (x) {
+			try {
+				proclaim.equal(x, 'foo=baz');
+				done();
+			} catch (e) {
+				done(e);
+			}
+		});
+	});
+}
