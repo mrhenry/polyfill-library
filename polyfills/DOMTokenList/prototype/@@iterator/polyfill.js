1b6beb9d9fe9655f7def5f0052e7932006ef897c
romainmenke <romainmenke@gmail.com>
2021-09-05 13:29:58 +0200

    fix DOMTokenList polyfills for older WebKit versions

--- polyfills/DOMTokenList/prototype/@@iterator/polyfill.js
+++ polyfills/DOMTokenList/prototype/@@iterator/polyfill.js
@@ -1,4 +1,18 @@
-/* global Symbol, ArrayIterator*/
-DOMTokenList.prototype[Symbol.iterator] = function () {
-	return new ArrayIterator(this);
-};
+/* global ArrayIterator */
+(function (global) {
+	global.DOMTokenList.prototype[global.Symbol.iterator] = function () {
+		return new ArrayIterator(this);
+	};
+
+	var e = document.createElement('span');
+	if (
+		e.classList &&
+		e.classList.constructor &&
+		e.classList.constructor.prototype &&
+		!e.classList.constructor.prototype[global.Symbol.iterator]
+	) {
+		e.classList.constructor.prototype[global.Symbol.iterator] = function () {
+			return new ArrayIterator(this);
+		}
+	}
+}(self));
