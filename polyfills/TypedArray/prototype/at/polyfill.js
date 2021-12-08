ab4868cd7742e2eb5e9262b7a48e540290d9bc5e
Jake Champion <me@jakechampion.name>
2021-12-08 14:33:35 +0000

    Use the private _setter and _getter methods for TypedArrays when in IE8 because we can't use add square-bracket notation support due to the lack of getters and setters in IE8

--- polyfills/TypedArray/prototype/at/polyfill.js
+++ polyfills/TypedArray/prototype/at/polyfill.js
@@ -1,6 +1,22 @@
-/* global CreateMethodProperty, Get, ToIntegerOrInfinity, ToString */
+/* global CreateMethodProperty, Uint8Array, ToIntegerOrInfinity, ToString */
 // 23.2.3.1. %TypedArray%.prototype.at ( index )
 (function () {
+	// In Internet Explorer 8 there is no support for square-bracket notation
+	// in the TypedArrays polyfill instead so we need to use the private `_getter` method
+	var typedArraysSupportIndexLookup = (function() {
+		var uint8 = new Uint8Array(2);
+		uint8[0] = 42;
+		return uint8[0] === 42
+	})
+
+	function getTypedArrayIndex(array, index) {
+		if (typedArraysSupportIndexLookup) {
+			return array[index];
+		} else {
+			return array._getter(index);
+		}
+	}
+
 	function at(index) {
 		// 1. Let O be the this value.
 		var O = this;
@@ -18,7 +34,8 @@
 		// 7. If k < 0 or k ≥ len, return undefined.
 		if (k < 0 || k >= len) return undefined;
 		// 8. Return ! Get(O, ! ToString(𝔽(k))).
-		return Get(O, ToString(k));
+		// return Get(O, ToString(k));
+		return getTypedArrayIndex(O, ToString(k));
 	}
 
 	if ('__proto__' in self.Int8Array.prototype) {
