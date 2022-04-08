9a696c2735e9738126028051781e1ba94634d8f6
mhassan1 <marc.j.hassan@gmail.com>
2022-04-08 16:43:00 -0400

    Fixes `HTMLSelectElement.prototype.remove` to include `Element.prototype.remove`

--- polyfills/Element/prototype/remove/tests.js
+++ polyfills/Element/prototype/remove/tests.js
@@ -38,3 +38,39 @@ it('can remove itself from nothing', function () {
 
 	proclaim.equal(element.childNodes.length, 0);
 });
+
+it('can remove a select', function () {
+	child.remove();
+
+	var select = document.createElement('select');
+	element.appendChild(select);
+
+	proclaim.equal(select.remove(), undefined);
+
+	proclaim.equal(element.childNodes.length, 0);
+});
+
+it('does not break the ability for a select to remove an option', function () {
+	child.remove();
+
+	var select = document.createElement('select');
+	var option = document.createElement('option');
+	element.appendChild(select);
+	select.appendChild(option);
+
+	proclaim.equal(select.remove(0), undefined);
+
+	proclaim.equal(select.childNodes.length, 0);
+	proclaim.equal(element.childNodes.length, 1);
+});
+
+it('does not remove a select when passed `undefined`', function () {
+	child.remove();
+
+	var select = document.createElement('select');
+	element.appendChild(select);
+
+	proclaim.equal(select.remove(undefined), undefined);
+
+	proclaim.equal(element.childNodes.length, 1);
+});
