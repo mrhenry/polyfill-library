8a3121f80abbdabc2259315f9973348559da7663
Romain Menke <11521496+romainmenke@users.noreply.github.com>
2023-05-09 22:55:15 +0200

    `Array.prototype.sort` : extra tests (#1288)
    * Array.prototype.sort : extra tests
    
    * add a test with sparse arrays

--- polyfills/Array/prototype/sort/tests.js
+++ polyfills/Array/prototype/sort/tests.js
@@ -89,12 +89,94 @@ it("sorts arrays with comparefn that returns non-number results", function () {
 	);
 });
 
-it("has a stable sort", function () {
-	var obj = {length:3, 0:2, 1:1,2:3};
+it("has a stable sort with array-like objects", function () {
+	var obj = { length: 3, 0: 2, 1: 1, 2: 3 };
 	proclaim.deepStrictEqual(
 		Array.prototype.sort.call(obj, function (a, b) {
 			return a - b;
 		}),
 		obj
 	);
+
+	proclaim.equal(obj[0], 1);
+	proclaim.equal(obj[1], 2);
+	proclaim.equal(obj[2], 3);
+});
+
+it("has a stable sort with arrays that contain duplicate values", function () {
+	var array = [
+		{ unique: 'a', sortValue: 1 },
+		{ unique: 'b', sortValue: 2 },
+		{ unique: 'c', sortValue: 0 },
+		{ unique: 'd', sortValue: 4 },
+		{ unique: 'e', sortValue: 2 },
+		{ unique: 'f', sortValue: 2 }
+	];
+
+	array.sort(function (a, b) {
+		return a.sortValue - b.sortValue;
+	});
+
+	proclaim.deepStrictEqual(array, [
+		{ unique: 'c', sortValue: 0 },
+		{ unique: 'a', sortValue: 1 },
+		{ unique: 'b', sortValue: 2 },
+		{ unique: 'e', sortValue: 2 },
+		{ unique: 'f', sortValue: 2 },
+		{ unique: 'd', sortValue: 4 }
+	]);
+});
+
+it("has a stable sort with arrays that contain duplicate values and comparefn occasionally returns NaN", function () {
+	var array = [
+		{ unique: 'a', sortValue: 1 },
+		{ unique: 'b', sortValue: 2 },
+		{ unique: 'c', sortValue: 0 },
+		{ unique: 'd', sortValue: 4 },
+		{ unique: 'e', sortValue: undefined },
+		{ unique: 'f', sortValue: 2 }
+	];
+
+	array.sort(function (a, b) {
+		return a.sortValue - b.sortValue;
+	});
+
+	proclaim.deepStrictEqual(array, [
+		{ unique: 'c', sortValue: 0 },
+		{ unique: 'a', sortValue: 1 },
+		{ unique: 'b', sortValue: 2 },
+		{ unique: 'd', sortValue: 4 },
+		{ unique: 'e', sortValue: undefined },
+		{ unique: 'f', sortValue: 2 }
+	]);
+});
+
+it("has a stable sort with arrays that contain duplicate values and is sparse", function () {
+	// eslint-disable-next-line no-sparse-arrays
+	var array = [
+		{ unique: 'a', sortValue: 1 },
+		{ unique: 'b', sortValue: 2 },
+		{ unique: 'c', sortValue: 0 },
+		,
+		{ unique: 'd', sortValue: 4 },
+		{ unique: 'e', sortValue: 2 },
+		{ unique: 'f', sortValue: 2 }
+	];
+
+	array.sort(function (a, b) {
+		return a.sortValue - b.sortValue;
+	});
+
+	// eslint-disable-next-line no-sparse-arrays
+	proclaim.deepStrictEqual(array, [
+		{ unique: 'c', sortValue: 0 },
+		{ unique: 'a', sortValue: 1 },
+		{ unique: 'b', sortValue: 2 },
+		{ unique: 'e', sortValue: 2 },
+		{ unique: 'f', sortValue: 2 },
+		{ unique: 'd', sortValue: 4 },
+		,
+	]);
+
+	proclaim.equal(array.length, 7);
 });
