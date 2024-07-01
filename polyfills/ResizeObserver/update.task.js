17a86bf38812e59ee786ab7efb52c26c74ecfd2e
Romain Menke <romainmenke@gmail.com>
2024-07-01 08:25:05 +0200

    fix resize observer

--- polyfills/ResizeObserver/update.task.js
+++ polyfills/ResizeObserver/update.task.js
@@ -1,9 +1,14 @@
 'use strict';
 
 var fs = require('fs');
+var diff = require('diff');
+var process = require('process');
 var path = require('path');
-var resizeObserverPolyfillOutput = path.resolve('polyfills/ResizeObserver/polyfill.js');
 
-var polyfill = fs.readFileSync(resizeObserverPolyfillOutput, 'utf-8');
-polyfill += ';self.ResizeObserverEntry = ResizeObserver.ResizeObserverEntry;self.ResizeObserver=ResizeObserver.ResizeObserver;'
-fs.writeFileSync(resizeObserverPolyfillOutput, polyfill, 'utf-8');
+var polyfill = fs.readFileSync(path.join(__dirname, './polyfill.js'), 'utf8');
+var patch = fs.readFileSync(path.join(__dirname, './patch.jsdiff'), 'utf8');
+
+var patched = diff.applyPatch(polyfill, patch);
+
+if (patched === false) {process.exit(1);}
+fs.writeFileSync(path.join(__dirname, './polyfill.js'), patched);
