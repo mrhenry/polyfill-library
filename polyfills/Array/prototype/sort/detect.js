36783e9c615d80da2769cbdfd8c52d3086542d11
mhassan1 <marc.j.hassan@gmail.com>
2024-07-11 13:35:21 -0400

    Improves stable sort detection in `Array.prototype.sort`

--- polyfills/Array/prototype/sort/detect.js
+++ polyfills/Array/prototype/sort/detect.js
@@ -1,5 +1,29 @@
 'sort' in Array.prototype && (function() {
 	// Check it does a stable sort
-	var obj = {length:3, 0:2, 1:1,2:3};
-	return Array.prototype.sort.call(obj, function(a,b) {return a-b}) === obj;
+	var arr = [
+		["z", "z"],
+		["a", "a"],
+		["z", "y"],
+		["a", "b"],
+		["z", "x"],
+		["a", "c"],
+		["z", "w"],
+		["a", "d"],
+		["z", "v"],
+		["a", "e"],
+		["z", "u"],
+		["a", "f"],
+		["z", "t"],
+		["a", "h"]
+	];
+	arr.sort(function (a, b) {
+		if (a[0] < b[0]) {
+			return -1;
+		}
+		if (a[0] > b[0]) {
+			return 1;
+		}
+		return 0;
+	});
+	return arr[0][1] === "a";
 }())
