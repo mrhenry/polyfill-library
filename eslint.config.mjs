7e9aa5d703a92ae8626d087734f0a253c96a7717
Romain Menke <romainmenke@gmail.com>
2024-09-23 23:18:59 +0200

    Revert "Switches `URL` polyfill to `whatwg-url` (#59)"
    This reverts commit c2a4d0b9489e25c3327e5251b534f1ea7db4d94a.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -61,7 +61,6 @@ const ignores = [
 	"polyfills/fetch/polyfill.js",
 	"polyfills/smoothscroll/polyfill.js",
 	"polyfills/~html5-elements/polyfill.js",
-	"polyfills/URL/polyfill.js",
 
 	// Vendored mocha
 	"test/polyfills/mocha/**",
