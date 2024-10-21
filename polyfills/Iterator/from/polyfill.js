5ed3b2714ab927e6a637701e4da8dd6847b5b1bf
mhassan1 <marc.j.hassan@gmail.com>
2024-10-21 16:43:25 -0400

    Fixes `Iterator.from`

--- polyfills/Iterator/from/polyfill.js
+++ polyfills/Iterator/from/polyfill.js
@@ -1,5 +1,5 @@
 /* global CreateMethodProperty, Iterator, IteratorHelpersUtils */
 // 3.1.1.2.2 Iterator.from ( O )
 CreateMethodProperty(Iterator, "from", function from(O) {
-	return IteratorHelpersUtils.iterator.from(this, O);
+	return IteratorHelpersUtils.iterator.from(O);
 });
