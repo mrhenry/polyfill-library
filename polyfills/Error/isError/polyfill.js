2e9c939e5da5ce74a2de3b80d5ab447697808447
mhassan1 <marc.j.hassan@gmail.com>
2025-02-20 22:16:38 -0500

    Remove broken `structuredClone` block

--- polyfills/Error/isError/polyfill.js
+++ polyfills/Error/isError/polyfill.js
@@ -1,5 +1,5 @@
 /* global CreateMethodProperty, Symbol, Type */
-// this is based on https://github.com/es-shims/Error.isError/blob/main/implementation.js
+// this is inspired by https://github.com/es-shims/Error.isError/blob/main/implementation.js
 // 20.5.2.1 Error.isError ( arg )
 CreateMethodProperty(Error, "isError", function isError(arg) {
 	// 1. If argument is not an Object, return false.
@@ -8,13 +8,6 @@ CreateMethodProperty(Error, "isError", function isError(arg) {
 	}
 	// 2. If argument has an [[ErrorData]] internal slot, return true.
 	// 3. Return false.
-	if ("structuredClone" in self) {
-		try {
-			return self.structuredClone(arg) instanceof Error;
-		} catch (e) {
-			return false;
-		}
-	}
 	if (!(Symbol.toStringTag in arg)) {
 		var str = Object.prototype.toString.call(arg);
 		return str === "[object Error]" || str === "[object DOMException]";
